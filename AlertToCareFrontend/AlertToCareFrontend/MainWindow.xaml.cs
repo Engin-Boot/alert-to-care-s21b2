b3e44785210a2a7339bb55ea4c935940540c18e4
Tripti Saini <triptisaini123@gmail.com>
2020-10-23 14:45:04 +0530

    Add initial structure

---
+++ AlertToCareFrontend/AlertToCareFrontend/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AlertToCareFrontend.Views;
+using System.Windows;
+
+namespace AlertToCareFrontend
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AlertToCareFrontend/AlertToCareFrontend/Views/HomeView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlertToCareFrontend.Views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void MonitorIcuButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConfigureButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
